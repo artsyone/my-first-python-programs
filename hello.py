6819fbc4f48c14be4b470fec90b9fbafefccd39d
artsyone <31486667+artsyone@users.noreply.github.com>
2017-08-30 14:23:49 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,21 @@
+
+# This says hello and greets a person by name
+#
+# Your Name 
+# Aug. 24, 2017
+
+
+
+print("Hello.")
+print("What is your name?")
+name = input ()
+print("It is good to meet you," + name + ".")
+
+print ("So " + name + "...")
+print("Where are you from?")
+where = input ()
+print("Thats interesting. Id like to vist " +  where + " one day .")
+
+print(" In " + where + " where do you like to eat")
+food = input()
+print ( "Thats so cool " + food + " sounds so good!")
---
+++ mad_lib.py
@@ -0,0 +1,78 @@
+print("Give me a adjective")
+adjective = input ()
+
+print("Give me a adjective")
+adjective_2 = input ()
+
+print("Give me a adjective")
+adjective_3 = input ()
+
+print("Give me a adverb")
+adverb  = input ()
+
+print("Give me a adverb")
+adverb_1 = input ()
+
+print("Give me a female first name ")
+fname = input ()
+
+print("Give me a last name")
+last_name = input ()
+
+print("Give me a male first name ")
+mname = input ()
+
+print("Give me a noun")
+noun = input ()
+
+print("Give me another noun")
+noun1 = input ()
+
+print("Give me another another  noun")
+noun2 = input ()
+
+print("Give me another another another noun ")
+noun3 = input ()
+
+print("Give me another another another another noun")
+noun4 = input ()
+
+print("Now give me a body part")
+body_part= input ()
+
+print(" Give me a body part make it plural")
+body_part2= input ()
+
+
+print("Give me a article of clothing plural")
+clothing = input ()
+
+
+print("Give me a plural noun")
+plural_noun = input ()
+
+print("Give me a plural noun")
+plural_noun1 = input ()
+
+print("\n")
+import time
+time.sleep(3)
+print("Creating your MadLib")
+import time
+time.sleep(5)
+print("\n")
+
+print("Newspaper critics agree that " + mname + " and "
++ fname + " is a comedy " + noun +  "" )
+print("\n")
+print( "It will tickle your funny " + body_part + "!-The Washington Z" )
+print("\n")
+print("Two "+ body_part2 + " up! -Ebert and " + last_name + "") 
+print("\n")
+print("A smart, "+ adjective + " and " + adjective_2 + " comedy.You not only laugh,"
+" but it brings " + plural_noun + " to your eyes. ")
+print("-The New Orleans Times- " + noun2 + "" )
+print("\n")
+print(" an " + adverb + " funny half-hour "+ noun3 + ";" + adverb_1 +
+" acted by a " + adjective_3 + " cast of all-star " + plural_noun1 + "."
+" -The Chicago Sun- " + noun4 + "" )
